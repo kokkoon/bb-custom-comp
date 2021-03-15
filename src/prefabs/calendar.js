2059fa0bb26f3f2e2aef41e58e5a691c232d46d9
Kok Koon Gan <kokkoon.gan@frasersproperty.com>
2021-03-15 11:57:56 +0800

    updated

--- src/prefabs/calendar.js
+++ src/prefabs/calendar.js
@@ -5,7 +5,52 @@
     structure: [
         {
             name: "calendar",
-            options: [],
+            options: [
+                {
+                    type: 'CUSTOM',
+                    label: 'Horizontal Alignment',
+                    key: 'horizontalAlignment',
+                    value: 'inherit',
+                    configuration: {
+                      as: 'BUTTONGROUP',
+                      dataType: 'string',
+                      allowedInput: [
+                        { name: 'None', value: 'inherit' },
+                        { name: 'Left', value: 'flex-start' },
+                        { name: 'Center', value: 'center' },
+                        { name: 'Right', value: 'flex-end' },
+                      ],
+                    },
+                  },
+                  {
+                    type: 'CUSTOM',
+                    label: 'Vertical Alignment',
+                    key: 'verticalAlignment',
+                    value: 'inherit',
+                    configuration: {
+                      as: 'BUTTONGROUP',
+                      dataType: 'string',
+                      allowedInput: [
+                        { name: 'None', value: 'inherit' },
+                        { name: 'Top', value: 'flex-start' },
+                        { name: 'Center', value: 'center' },
+                        { name: 'Bottom', value: 'flex-end' },
+                      ],
+                    },
+                  },
+                  {
+                    value: ['0rem', '0rem', '0rem', '0rem'],
+                    label: 'Outer space',
+                    key: 'outerSpacing',
+                    type: 'SIZES',
+                  },
+                  {
+                    value: ['M', 'M', 'M', 'M'],
+                    label: 'Inner space',
+                    key: 'innerSpacing',
+                    type: 'SIZES',
+                  },
+            ],
             descendants: []
         }
     ],
